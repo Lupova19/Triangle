d4aebb7858c9928aac4502cfec33cca7d862d05b
Lupova19 <nadiqlupova@gmail.com>
2024-09-25 23:13:13 +0300

    first commit

---
+++ Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи данни за триъгълник едно:");
+            Triangle tr1 = new Triangle(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            Triangle tr2 = new Triangle(5, 4, 2);
+            Triangle tr3 = new Triangle(8, 7, 3);
+            Triangle tr4 = new Triangle(9, 6, 4);
+
+             Console.WriteLine($"Триъгълник 1: S={tr1.Area():f2}, P={tr1.Perimeter()}");
+             Console.WriteLine($"Триъгълник 2: S={tr2.Area():f2}, P={tr2.Perimeter()}");
+             Console.WriteLine($"Триъгълник 3: S={tr3.Area():f2}, P={tr3.Perimeter()}");
+             Console.WriteLine($"Триъгълник 4: S={tr4.Area():f2}, P={tr4.Perimeter()}");
+        }
+    }
+}
---
+++ Triangle/Triangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        
+        public Triangle(double a, double b, double c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.A}{this.B}{this.C}";
+        }
+
+        public double Area()
+        {
+            return c = a * b / 2;
+
+        }
+
+        public double Perimeter()
+        {
+            return a + b + c;
+        }
+    }
+}
